8f600bb02280ebadc09db9d21a17eac5aff1aba2
Quentin Read <quentin.read@gmail.com>
2021-05-04 13:58:32 -0400

    update lue plots

--- light_use_efficiency.R
+++ light_use_efficiency.R
@@ -3,7 +3,7 @@
 library(tidyverse)
 library(forestscaling)
 
-load("data/rawdataobj1995.RData")
+load("ForestLight/data/rawdataobj_withimputedproduction.RData")
 
 # Load light use efficiency extracted data (coefficients and fitted slopes)
 load("ForestLight/data/data_piecewisefits/lue_info.RData")
@@ -47,7 +47,7 @@ colors <- RColorBrewer::brewer.pal(3, 'RdYlBu')
 # Hexbin plot with cloud bins
 ggplot() +
   geom_hex(data = dat_points %>% filter(!fg %in% "unclassified"), mapping = aes(x = dbh_corr, y = LUE)) +
-  geom_pointrange(data = luebin %>% filter(!fg %in% "unclassified"), mapping = aes(x = bin_midpoint, y = median, ymin = q025, ymax = q975)) +
+  geom_pointrange(data = luebin %>% filter(!fg %in% "unclassified", mean_n_individuals > 20, complete.cases(.)), mapping = aes(x = bin_midpoint, y = median, ymin = q025, ymax = q975)) +
   facet_wrap(~ fg) +
   scale_x_log10(name = 'diameter (cm)') +
   scale_y_log10(name = parse(text = 'LUE~(kg~y^-1~W^-1)')) +
@@ -57,13 +57,18 @@ ggplot() +
 # Hexbin plot with fits
 ggplot() +
   geom_hex(data = dat_points %>% filter(!fg %in% "unclassified"), mapping = aes(x = dbh_corr, y = LUE)) +
+  geom_ribbon(data = lue_fitted, mapping = aes(x = dbh, ymin = q025, ymax = q975), alpha = 0.5, fill = 'black') +
   geom_line(data = lue_fitted, mapping = aes(x = dbh, y = q50)) +
-  geom_line(data = lue_fitted, mapping = aes(x = dbh, y = q025), linetype = 'dotted') +
-  geom_line(data = lue_fitted, mapping = aes(x = dbh, y = q975), linetype = 'dotted') +
   facet_wrap(~ fg) +
   scale_x_log10(name = 'diameter (cm)') +
   scale_y_log10(name = parse(text = 'LUE~(kg~y^-1~W^-1)')) +
   scale_fill_gradient2(low = colors[3], mid = colors[2], high = colors[1], trans = 'log10') +
   theme_bw()
 
-
+# Coefficient plots
+ggplot(lue_slopes, aes(x = fg, y = `50%`, ymin = `2.5%`, ymax = `97.5%`)) +
+  geom_hline(yintercept = 0, linetype = 'dotted', size = 2) +
+  geom_point(size = 2) +
+  geom_errorbar(width = 0.2) +
+  theme_bw() +
+  labs(y = 'LUE scaling exponent')
